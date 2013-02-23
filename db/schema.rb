94223cf4167e02da36486c03a44a3d23e9185e90
Pardees Safizadeh <pardees.safizadeh@gmail.com>
2013-02-23 15:03:19 -0500

    Added Users Feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130223191631) do
+ActiveRecord::Schema.define(:version => 20130223200116) do
 
   create_table "topics", :force => true do |t|
     t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
